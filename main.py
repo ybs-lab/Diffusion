8ee2db638e3f4fc4326ffa7b6ba9b55a099b5269
Amit Federbush <afederbush@gmail.com>
2021-07-21 01:26:45 +0300

    data + stam .py files

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from math_stam import my_sum2
+
+def read_CSV(P1,P2,salinity):
+    pass
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(x(1,1))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ math_stam.py
@@ -0,0 +1,5 @@
+def my_sum(x,y):
+    return x+y
+
+def my_sum2(x,y):
+    return my_sum(x,2*y)
